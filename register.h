e6bd2c1e4fc32e26958699041381469b03d91309
siddhu70707 <siddhant.kuswaha70707.com>
2026-09-03 20:33:44 +0530

    added proper classes for ALU and reg file

---
+++ register.h
@@ -0,0 +1,15 @@
+#ifndef REGISTER_H
+#define REGISTER_H
+
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+class registers{
+    public:
+        uint16_t value;
+        bool we, re;
+
+};
+
+#endif
